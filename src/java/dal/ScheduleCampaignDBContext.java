8c7af0581308595735493e8fb3742f76334ef109
quankmksnb <quannguyenkm16122004@gmail.com>
2024-10-31 18:57:23 +0700

    Update project

--- src/java/dal/ScheduleCampaignDBContext.java
+++ src/java/dal/ScheduleCampaignDBContext.java
@@ -3,11 +3,37 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package dal;
-
+import java.util.ArrayList;
+import models.ScheduleCampaign;
 /**
  *
  * @author Admin
  */
-public class ScheduleCampaignDBContext {
+public class ScheduleCampaignDBContext extends DBContext<ScheduleCampaign> {
+
+    @Override
+    public void insert(ScheduleCampaign model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void update(ScheduleCampaign model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void delete(ScheduleCampaign model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public ArrayList<ScheduleCampaign> list() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public ScheduleCampaign get(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
